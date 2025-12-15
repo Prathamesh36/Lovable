1cf79e109a93f60a1689cfdd98c1c43c5c7a6811
Prathamesh <pdpatil1006@gmail.com>
2025-12-15 22:54:49 +0530

    Implement authentication service with signup and login functionality; add JWT token generation and security configuration

--- Lovable/src/main/java/com/portfolio/projects/service/impl/AuthServiceImpl.java
+++ Lovable/src/main/java/com/portfolio/projects/service/impl/AuthServiceImpl.java
@@ -1,20 +1,62 @@
 package com.portfolio.projects.service.impl;
 
+import com.portfolio.projects.Repository.UserRepository;
 import com.portfolio.projects.dto.auth.AuthResponse;
 import com.portfolio.projects.dto.auth.LoginRequest;
 import com.portfolio.projects.dto.auth.SignupRequest;
+import com.portfolio.projects.entity.User;
+import com.portfolio.projects.error.BadRequestException;
+import com.portfolio.projects.mapper.UserMapper;
+import com.portfolio.projects.security.AuthUtil;
 import com.portfolio.projects.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthServiceImpl implements AuthService {
+
+    UserRepository userRepository;
+
+    UserMapper userMapper;
+
+    PasswordEncoder passwordEncoder;
+
+    AuthUtil authUtil;
+
+    AuthenticationManager authenticationManager;
     @Override
     public AuthResponse signup(SignupRequest request) {
-        return null;
+        userRepository.findByUsername(request.username()).ifPresent(user ->{
+            throw new BadRequestException("User already exists with username: "+ request.username());
+        });
+
+        User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user = userRepository.save(user);
+
+        String token = authUtil.generateAccessToken(user);
+
+        return new AuthResponse(token, userMapper.toUserProfileResponse(user));
     }
 
     @Override
     public AuthResponse login(LoginRequest request) {
-        return null;
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.username(), request.password())
+        );
+
+        User user = (User) authentication.getPrincipal();
+
+        String token = authUtil.generateAccessToken(user);
+
+        return new AuthResponse(token, userMapper.toUserProfileResponse(user));
     }
 }
--- Lovable/src/main/java/com/portfolio/projects/service/impl/UserServiceImpl.java
+++ Lovable/src/main/java/com/portfolio/projects/service/impl/UserServiceImpl.java
@@ -1,13 +1,32 @@
 package com.portfolio.projects.service.impl;
 
+import com.portfolio.projects.Repository.UserRepository;
 import com.portfolio.projects.dto.auth.UserProfileResponse;
+import com.portfolio.projects.error.ResourceNotFoundException;
 import com.portfolio.projects.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    UserRepository userRepository;
+
     @Override
     public UserProfileResponse getProfile(Long userId) {
         return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User", username));
     }
 }
